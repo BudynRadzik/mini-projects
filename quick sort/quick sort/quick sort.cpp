d00902038bad8836ab5905aedf65779f7d1713f8
BudynRadzik <118636738+BudynRadzik@users.noreply.github.com>
2023-05-28 22:04:31 +0200

    guick sort
    first try of quick sort ( doesn t work , need changes)

---
+++ quick sort/quick sort/quick sort.cpp
@@ -0,0 +1,62 @@
+// quick sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+
+
+void quickSort(int numbers[],int begin, int end)
+{
+    int i = begin;
+    int j = end;
+    int pivot = (numbers[(begin+end)/2]);
+    //std::cout << "pivot  "<<pivot << std::endl;
+    int temp=0;
+ 
+    do 
+    {
+        while (numbers[i] < pivot) i++;
+        while (numbers[j] > pivot) j--;
+        if (i <= j)
+        {
+            temp = numbers[i];
+              numbers[i] = numbers[j];
+              numbers[j] = temp;
+              i++; j++;
+
+        }
+
+    } while (i <= j);
+
+    if (j > begin) quickSort(numbers, begin, j);
+    if (i < end) quickSort(numbers, i, end);
+  }
+    
+
+
+    
+        
+        
+        
+        
+
+
+
+ 
+int main()
+{
+    int arr[] = { 1000,2,43,100,1,2,3,4,5,2,43,100,1,2,55,3,4,5 };
+    int arr_size = sizeof(arr);
+    quickSort(arr,0,arr_size-1);
+
+
+
+
+
+    for (auto x : arr)
+    {
+        std::cout << x << " ";
+    }
+    return 0;
+}
+
+
