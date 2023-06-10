66d4c4f6e5a00331049961802fd199d37e949eeb
BudynRadzik <118636738+BudynRadzik@users.noreply.github.com>
2023-06-10 20:49:56 +0200

    quick sort update

--- quick sort/quick sort/quick sort.cpp
+++ quick sort/quick sort/quick sort.cpp
@@ -8,8 +8,9 @@ void quickSort(int numbers[],int begin, int end)
 {
     int i = begin;
     int j = end;
-    int pivot = (numbers[(begin+end)/2]);
-    //std::cout << "pivot  "<<pivot << std::endl;
+    
+    int pivot = numbers[(begin+end)/2];
+    std::cout << "pivot  "<<pivot << std::endl;
     int temp=0;
  
     do 
@@ -21,7 +22,7 @@ void quickSort(int numbers[],int begin, int end)
             temp = numbers[i];
               numbers[i] = numbers[j];
               numbers[j] = temp;
-              i++; j++;
+             // i++; j++;
 
         }
 
@@ -45,17 +46,18 @@ void quickSort(int numbers[],int begin, int end)
 int main()
 {
     int arr[] = { 1000,2,43,100,1,2,3,4,5,2,43,100,1,2,55,3,4,5 };
-    int arr_size = sizeof(arr);
-    quickSort(arr,0,arr_size-1);
+    int arr_size = 18;
+    std::cout << arr_size << std::endl;
+    quickSort(arr,0,17);
 
 
-
-
-
-    for (auto x : arr)
+    for (int i = 0; i < sizeof(arr); i++)
     {
-        std::cout << x << " ";
+        std::cout << arr[i];
     }
+
+
+    
     return 0;
 }
 
