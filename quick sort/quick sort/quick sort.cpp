cc357b53ba223cf79926b010ee52193025bf9f8c
BudynRadzik <118636738+BudynRadzik@users.noreply.github.com>
2023-06-10 20:54:56 +0200

    quick sort update

--- quick sort/quick sort/quick sort.cpp
+++ quick sort/quick sort/quick sort.cpp
@@ -1,64 +1,49 @@
 // quick sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-
 #include <iostream>
 
+void quicksort(int temp_tab[], int begin, int end);
 
-void quickSort(int numbers[],int begin, int end)
-{
-    int i = begin;
-    int j = end;
-    
-    int pivot = numbers[(begin+end)/2];
-    std::cout << "pivot  "<<pivot << std::endl;
-    int temp=0;
- 
-    do 
-    {
-        while (numbers[i] < pivot) i++;
-        while (numbers[j] > pivot) j--;
-        if (i <= j)
-        {
-            temp = numbers[i];
-              numbers[i] = numbers[j];
-              numbers[j] = temp;
-             // i++; j++;
-
-        }
-
-    } while (i <= j);
-
-    if (j > begin) quickSort(numbers, begin, j);
-    if (i < end) quickSort(numbers, i, end);
-  }
-    
-
-
-    
-        
-        
-        
-        
-
-
-
- 
 int main()
 {
-    int arr[] = { 1000,2,43,100,1,2,3,4,5,2,43,100,1,2,55,3,4,5 };
-    int arr_size = 18;
-    std::cout << arr_size << std::endl;
-    quickSort(arr,0,17);
+    int array[] = { 1,9,2,8,3,7,4,6,5,0 };
+    int array_size = sizeof(array) / sizeof(int);
+    std::cout << "Array size: " << array_size << std::endl;
+    std::cout << "-------------------------" << std::endl;
+    std::cout << "Array before quick sort:" << std::endl;
 
-
-    for (int i = 0; i < sizeof(arr); i++)
+    for (int i = 0; i < array_size; i++)
     {
-        std::cout << arr[i];
+        std::cout << array[i] << " ";
     }
+    std::cout << "\n";
+    std::cout << "-------------------------" << std::endl;
+    std::cout << "Array after quick sort:" << std::endl;
 
+    quicksort(array, 0, array_size - 1);
 
-    
+    for (int i = 0; i < array_size; i++)
+    {
+        std::cout << array[i] << " ";
+    }
     return 0;
 }
-
+void quicksort(int temp_tab[], int begin, int end) {
+    int i = begin;
+    int j = end;
+    int pivot = temp_tab[(begin + end) / 2];
+    do {
+        while (temp_tab[i] < pivot) i++;
+        while (temp_tab[j] > pivot) j--;
+        if (i <= j) {
+            int temp = temp_tab[i];
+            temp_tab[i] = temp_tab[j];
+            temp_tab[j] = temp;
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    if (begin < j) quicksort(temp_tab, begin, j);
+    if (end > i) quicksort(temp_tab, i, end);
+}
 
