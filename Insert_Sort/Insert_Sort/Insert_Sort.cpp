a6ca2b56f8ecf7e53493e19de9d31ea3de1c53fc
BudynRadzik <118636738+BudynRadzik@users.noreply.github.com>
2023-06-11 23:22:45 +0200

    Update Insert_Sort.cpp

--- Insert_Sort/Insert_Sort/Insert_Sort.cpp
+++ Insert_Sort/Insert_Sort/Insert_Sort.cpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+
+void InsertSort(int temp_array[],int arr_size);
+
 int main()
 {
     int array[] = { 1,9,2,8,3,7,4,6,5,0 }; //10
@@ -18,7 +21,16 @@ int main()
     std::cout << "\n";
     std::cout << "-------------------------" << std::endl;
     std::cout << "Array after insert sort:" << std::endl;
+    InsertSort(array, array_size);
+    for (int i = 0; i < array_size; i++)
+    {
+        std::cout << array[i] << " ";
+    }
 
     return 0;
 }
 
+void InsertSort(int temp_arr[], int arr_size) 
+{
+    
+}
