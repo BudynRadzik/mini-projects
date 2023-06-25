0dcc010aae0670bd216a395d1fe9d23f5508a0d9
BudynRadzik <118636738+BudynRadzik@users.noreply.github.com>
2023-06-25 21:41:38 +0200

    added swap with xor
    swap two integers with temp variable
    swap two integers  without temp variable
    swap two integers  without temp variable using xor

--- Pointers/Pointers/Pointers.cpp
+++ Pointers/Pointers/Pointers.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 using namespace std;
-void swap_with_pointers(int*ptr_a,int*ptr_b)
+void swap_without_temp(int*ptr_a,int*ptr_b)
 {
     *ptr_a = *ptr_a + *ptr_b;
     *ptr_b = *ptr_a - *ptr_b;
@@ -12,30 +12,39 @@ void swap_with_pointers(int*ptr_a,int*ptr_b)
 
 
 }
-void swap_with_temp(int a, int b)
+void swap_with_temp(int *a, int *b)
 {
-    int temp = a;
-    a = b;
-    b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
+void swap_with_xor(int* a, int* b)
+{
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
 int main()
 {
     int a = 10;
     int b = 20;
-    int*a = &a;
-    int*b = &b;
-
     
-
-    printf("Integers before swap: a %i , b %i \n",a,b);
+    printf("Integers before swap: a %i , b %i \n", a, b);
     printf("------------------------------------\n");
     printf("Integers after swap using pointers:\n");
     printf("------------------------------------\n");
 
-    swap_with_pointers(int* ptr_a, int* ptr_b);
-    void swap_with_temp(int a, int b);
-   
+    swap_with_temp(&a, &b);
+    printf("Integers after swap with temp: a %i , b %i \n", a, b);
+    //printf("Integers before swap: a %i , b %i \n", a, b);
+
+    swap_without_temp(&a, &b);
+    printf("Integers after swap without temp: a %i , b %i \n", a, b);
+    //binarnie 
+
+    swap_with_xor(&a, &b);
+    printf("Integers after swap with xor: a %i , b %i \n", a, b);
 
     return 0;
 }
